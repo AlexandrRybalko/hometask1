c1019edb2dca20480bc1161d2597f3723ce7daaa
AlexsandrRybalko <rybalkosanya20@gmail.com>
2020-08-22 20:23:01 +0300

    added hw1

---
+++ ConsoleApp_Homework_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Library_Homework_1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApp_Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Enter a");
+            while(!Double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Input is not valid");
+                Console.WriteLine("Enter a");
+            }
+            Console.WriteLine("Enter b");
+            while (!Double.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Input is not valid");
+                Console.WriteLine("Enter b");
+            }
+            Console.WriteLine("Enter c");
+            while (!Double.TryParse(Console.ReadLine(), out c))
+            {
+                Console.WriteLine("Input is not valid");
+                Console.WriteLine("Enter c");
+            }
+
+            Console.WriteLine();
+
+            Decomposer decomposer = new Decomposer(a, b, c);
+            decomposer.Resolve();
+
+            /*if (Decomposer.CountDiscriminant(a, b, c) > 0)
+            {
+                Console.WriteLine($"x1 = {Decomposer.FindX1(Decomposer.CountDiscriminant(a, b, c), a, b)}, x2 = {Decomposer.FindX2(Decomposer.CountDiscriminant(a, b, c), a, b)}");
+                Console.WriteLine(Decomposer.Decompose(a, Decomposer.FindX1(Decomposer.CountDiscriminant(a, b, c), a, b), Decomposer.FindX2(Decomposer.CountDiscriminant(a, b, c), a, b)));
+                
+            }
+            else if(Decomposer.CountDiscriminant(a, b, c) == 0)
+            {
+                Console.WriteLine($"x1 = x2 = {Decomposer.FindSingleX(a, b)}");
+                Console.WriteLine(Decomposer.Decompose(a, Decomposer.FindSingleX(a, b), Decomposer.FindSingleX(a, b)));
+            }
+            else
+            {
+                Console.WriteLine("The equation has no roots");
+            }*/
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Library1/Decomposer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Homework_1
+{
+    public class Decomposer
+    {
+        private double a, b, c;
+        private double discriminant;
+        private double[] roots;
+
+        public Decomposer(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            roots = new double[2];
+        }
+
+        private void CountDiscriminant()
+        {
+            discriminant = b * b - 4 * a * c;
+        }
+
+        private bool HasRealRoots()
+        {
+            return discriminant >= 0;
+        }
+
+        private void FindRoots()
+        {
+            roots[0] = (-1 * b + Math.Sqrt(discriminant)) / (2 * a);
+            roots[1] = (-1 * b - Math.Sqrt(discriminant)) / (2 * a);
+        }
+
+        private void PrintRoots()
+        {
+            Console.WriteLine($"x1={roots[0]}, x2={roots[1]}");
+        }
+
+        private void PrintMessage()
+        {
+            Console.WriteLine("No roots");
+        }
+
+        private void Decompose()
+        {
+            string result = $"{a}(x";
+            if(roots[0] < 0)
+            {
+                result += $"+{-1 * roots[0]})(x";
+            }
+            if (roots[0] > 0)
+            {
+                result += $"-{roots[0]})(x";
+            }
+            if (roots[1] < 0)
+            {
+                result += $"+{-1 * roots[1]})";
+            }
+            if (roots[1] > 0)
+            {
+                result += $"-{roots[1]})";
+            }
+            Console.WriteLine(result);
+        }
+
+        public void Resolve()
+        {
+            CountDiscriminant();
+            if (HasRealRoots())
+            {
+                FindRoots();
+                PrintRoots();
+                Decompose();
+            }
+            else
+            {
+                PrintMessage();
+            }
+        }
+
+    }
+}
